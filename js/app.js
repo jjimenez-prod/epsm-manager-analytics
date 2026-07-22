13844c38f169ac276ce63e92fbb1fb0ac2500f07
jjimenez-prod <j.j.ar4neda@gmail.com>
2026-07-22 11:49:05 +0200

    First Draft 0.1

---
+++ js/app.js
@@ -0,0 +1,6 @@
+// =====================================================
+// APPLICATION ENTRY POINT
+// =====================================================
+'use strict';
+
+// Interfaz estática de la primera iteración. Aquí se conectarán los datos de Analytics.
